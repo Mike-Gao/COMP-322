ee9d761e5c5e76ab42dad0e7f5e6d88c7c6b4bb9
Mike Gao <gao@cim.mcgill.ca>
2020-01-25 21:29:41 -0500

    Assignment 1

---
+++ Assignments/1/functions.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mike Gao on 2020-01-25.
+//
+
+#include "functions.h"
---
+++ Assignments/1/functions.h
@@ -0,0 +1,14 @@
+//
+// Created by Mike Gao on 2020-01-25.
+//
+
+#ifndef INC_1_FUNCTIONS_H
+#define INC_1_FUNCTIONS_H
+
+
+class functions {
+
+};
+
+
+#endif //INC_1_FUNCTIONS_H
