8fe4cd38f0cdfc110543add782220622ef199f31
Mike Gao <gao@cim.mcgill.ca>
2020-03-06 18:47:31 -0500

    Assignment 2 Basecode

---
+++ Assignments/2/blackjack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mike Gao on 2020-03-06.
+//
+
+#include "blackjack.h"
---
+++ Assignments/2/blackjack.h
@@ -0,0 +1,35 @@
+//
+// Created by Mike Gao on 2020-03-06.
+//
+
+#ifndef INC_2_BLACKJACK_H
+#define INC_2_BLACKJACK_H
+class Card {
+
+};
+
+class Hand {
+
+};
+
+class Deck {
+
+};
+
+class AbstractPlayer{
+
+};
+
+class HumanPlayer{
+
+};
+
+class computerPlayer{
+
+};
+
+class BlackJackGame{
+    
+};
+
+#endif //INC_2_BLACKJACK_H
